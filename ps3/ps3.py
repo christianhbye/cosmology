0c01a756564b89bd5bb4289f155e8b55e81d5ad7
Christian Hellum Bye <chbye@berkeley.edu>
2022-09-26 11:05:16 -0700

    initial commit

---
+++ ps3/ps3.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+c = 299792.458  # km/s
+
+
+def lum_dist(z, om_m, om_l, h=0.7):
+    """
+    Compute the luminosity distance at a redshift z, given the present values
+    of the density parameters Omega_m and Omega_Lambda and of the Hubble
+    parameter h, where h is defined such that H0 = 100h.
+    """
+    H0 = 100 * h  # in km/Mpc/s
+    q0 = om_m / 2 - om_l  # deceleration parameter
+    r = c / H0 * (z - (1 + q0) * z**2 / 2)  # small-z approximation
+    return r
+
+
+def dm(dL):
+    """
+    Compute the distance modulus given the luminoisty distance dL in Mpc.
+    """
+    return 5 * (np.log10(dL) + 5)  # dL in Mpc so 10pc = 1e-5 Mpc
+
+
+if __name__ == "__main__":
+    z, mu, sig = np.loadtxt("hstsn.data", skiprows=5, usecols=(1, 2, 3)).T
+    omegas = [[0.27, 0.73], [0.27, 0.0], [1.0, 0.0]]
+    z_grid = np.linspace(1e-3, 1.2 * z.max(), num=200)
+    plt.figure()
+    plt.xlabel("$z$")
+    plt.ylabel("$\\mu$")
+    for i in range(len(omegas)):
+        om_m, om_l = omegas[i]
+        dms = dm(lum_dist(z_grid, om_m, om_l))
+        label = "$(\\Omega_{0, m}, \\Omega_{0, \Lambda}) =$"
+        label += f"({om_m:.2f}, {om_l:.2f})"
+        plt.plot(z_grid, dms, label=label)
+    plt.legend()
+    plt.savefig("2a.eps")
+
+    plt.errorbar(
+        z,
+        mu,
+        yerr=sig,
+        fmt="none",
+        capsize=2,
+        label="Riess et al (2007)",
+        c="k",
+    )
+    plt.legend()
+    plt.savefig("2b.eps")
