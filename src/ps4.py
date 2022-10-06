27293ce99e18669f1ef6e67a7e8d036686863f14
Christian Hellum Bye <chbye@berkeley.edu>
2022-10-06 16:19:23 -0700

    write code to compute H0 vs t0

--- src/ps4.py
+++ src/ps4.py
@@ -14,6 +14,16 @@ def t0_flat(omega_m, h):
     t = 2 / (3 * H0 * np.sqrt(omega_l)) * np.log(log_arg)
     return t
 
+def H0_flat(omega_m, t0):
+    """
+    Invert t0(omega_m, H0) to solve for H0. This function exploits that t0 and
+    H0 are inversely proportional in the flat universe - hence the inverse of
+    the t0_flat function can be obtained by swapping the roles of t0 and H0
+    """
+    h = t0_flat(omega_m, t0)
+    H0 = 100 * h
+    return H0
+
 
 if __name__=="__main__":
     HW_DIR = "../ps4/"
@@ -36,8 +46,25 @@ if __name__=="__main__":
     plt.savefig(HW_DIR + "q1a.eps", bbox_inches="tight")
 
     # 1b
-    omega_m = np.linspace(1e-6, 1, num=100)
+    omega_m = np.linspace(1e-6, 1-1e-6, num=100)
     age = t0_flat(omega_m, 1)
     plt.plot(omega_m, age, label="$\\Omega_{0, \\Lambda}=1-\\Omega_{0, m} $")
     plt.legend()
     plt.savefig(HW_DIR + "q1b.eps", bbox_inches="tight")
+
+    # 1c
+    t0_vals = np.array([11, 13.8, 18])  # Gyr
+    H0_vals = H0_flat(omega_m[None, :], t0_vals[:, None])
+    # convert H0 from 1/Gyr to km/s/Mpc
+    H0_vals /= 1e9 * 3.154e7  # 1/Gyr -> 1/s
+    H0_vals *= 3.086e19  # convert to km/Mpc
+    plt.figure()
+    for i in range(len(t0_vals)):
+        plt.plot(omega_m, H0_vals[i], label=f"$t_0 = {t0_vals[i]:.1f}$ Gyr") 
+    plt.xlabel("$\Omega_{0, m}$")
+    plt.ylabel("$H_0 [\mathrm{km s}^{-1} \mathrm{Mpc}^{-1}]$")
+    plt.legend()
+    plt.grid()
+    plt.xlim(omega_m.min(), omega_m.max())
+    plt.yscale("log")
+    plt.savefig(HW_DIR + "q1c.eps", bbox_inches="tight")
