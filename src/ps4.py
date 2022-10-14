da8037f3a345307e664505f82f6db88ed39d773f
Christian Hellum Bye <chbye@berkeley.edu>
2022-10-14 16:43:35 -0700

    blacken

--- src/ps4.py
+++ src/ps4.py
@@ -3,6 +3,7 @@ import numpy as np
 
 from ps2 import t0 as t0_matter
 
+
 def t0_flat(omega_m, h):
     """
     Compute t0 for a flat universe given omega_m and h. Since the universe
@@ -10,9 +11,10 @@ def t0_flat(omega_m, h):
     """
     omega_l = 1 - omega_m
     H0 = 100 * h
-    log_arg = (1 + np.sqrt(omega_l)) / np.sqrt(omega_m) 
+    log_arg = (1 + np.sqrt(omega_l)) / np.sqrt(omega_m)
     t = 2 / (3 * H0 * np.sqrt(omega_l)) * np.log(log_arg)
     return t
+
 
 def H0_flat(omega_m, t0):
     """
@@ -24,6 +26,7 @@ def H0_flat(omega_m, t0):
     H0 = 100 * h
     return H0
 
+
 def t_vs_z(z, omega_m, omega_lambda, h=7.2e-4):
     """
     Compute time corresponding to redshift z.
@@ -31,7 +34,7 @@ def t_vs_z(z, omega_m, omega_lambda, h=7.2e-4):
     H0 = 100 * h
     a = 1 / (1 + z)
     if omega_m == 1:  # flat, matter dominated
-        return 2 / (3 * H0) * a ** (3/2)
+        return 2 / (3 * H0) * a ** (3 / 2)
     elif omega_lambda + omega_m == 1:  # flat with lambda
         u = np.sqrt(omega_m / a**3 + omega_lambda)
         log_arg = u + np.sqrt(omega_lambda)
@@ -39,15 +42,15 @@ def t_vs_z(z, omega_m, omega_lambda, h=7.2e-4):
         return np.log(log_arg) / (3 * H0 * np.sqrt(omega_lambda))
     elif omega_lambda == 0 and omega_m < 1:  # open, matter dominated
         coshx = 1 + 2 * a * (1 - omega_m) / omega_m
-        sinhx = np.sqrt(coshx ** 2 - 1)
+        sinhx = np.sqrt(coshx**2 - 1)
         x = np.arccosh(coshx)
-        t = 1 / (2 * H0) * omega_m / (1-omega_m)**(3/2) * (sinhx - x)
+        t = 1 / (2 * H0) * omega_m / (1 - omega_m) ** (3 / 2) * (sinhx - x)
         return t
     else:
         raise NotImplementedError
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     HW_DIR = "../ps4/"
 
     # t0 always scales with 1/h so if we set h=1, we effectively get 1/h units
@@ -68,7 +71,7 @@ if __name__=="__main__":
     plt.savefig(HW_DIR + "q1a.eps", bbox_inches="tight")
 
     # 1b
-    omega_m = np.linspace(1e-6, 1-1e-6, num=100)
+    omega_m = np.linspace(1e-6, 1 - 1e-6, num=100)
     age = t0_flat(omega_m, 1)
     plt.plot(omega_m, age, label="$\\Omega_{0, \\Lambda}=1-\\Omega_{0, m} $")
     plt.legend()
@@ -82,14 +85,14 @@ if __name__=="__main__":
     H0_vals *= 3.086e19  # convert to km/Mpc
 
     # H0 for matter dominated universe with t0 >11.5 Gyr
-    H0_max_matter = 2 / (3*11.5)
+    H0_max_matter = 2 / (3 * 11.5)
     H0_max_matter /= 1e9 * 3.154e7
     H0_max_matter *= 3.086e19
     print(H0_max_matter)
 
     plt.figure()
     for i in range(len(t0_vals)):
-        plt.plot(omega_m, H0_vals[i], label=f"$t_0 = {t0_vals[i]:.1f}$ Gyr") 
+        plt.plot(omega_m, H0_vals[i], label=f"$t_0 = {t0_vals[i]:.1f}$ Gyr")
     plt.axhline(70, ls="--", c="k")
     plt.fill_between(
         omega_m, H0_vals[0], color="lightsteelblue", label="$t_0 \geq 11$ Gyr"
@@ -113,8 +116,8 @@ if __name__=="__main__":
     # 2
     z_cmb = 1100
     z_gal = 13
-    om_ms = [0.32, 0.32, 1.]
-    om_ls = [0.68, 0., 0.]
+    om_ms = [0.32, 0.32, 1.0]
+    om_ls = [0.68, 0.0, 0.0]
     for z in [z_cmb, z_gal]:
         for i in range(3):
             om = om_ms[i]
@@ -122,29 +125,28 @@ if __name__=="__main__":
             print(om, ol, z)
             print(t_vs_z(z, om, ol))
 
-
     # 3
     def flat_th(z, L=20):
-        y = 1.2e-3 * (1+z)
-        y /= 2 - 2/np.sqrt(1+z)
+        y = 1.2e-3 * (1 + z)
+        y /= 2 - 2 / np.sqrt(1 + z)
         return y * L
 
     def integral(z, omega_m):
         a = np.sqrt(np.abs(omega_m - 1))
         y = 2 / a
-        u = np.sqrt(omega_m * (1+z) + (1-omega_m))
+        u = np.sqrt(omega_m * (1 + z) + (1 - omega_m))
         y *= np.arctan2(u, a) - np.arctan2(1, a)
         return y
 
     def open_th(z, om_m=0.3, L=20):
-        y = 1.2e-3 * (1+z) * np.sqrt(1-om_m) * L
-        x = np.sqrt(1-om_m) * integral(z, om_m)
+        y = 1.2e-3 * (1 + z) * np.sqrt(1 - om_m) * L
+        x = np.sqrt(1 - om_m) * integral(z, om_m)
         y /= np.sinh(x)
         return y
-    
-    def closed_th(z, om_m=3., L=20):
-        y = 1.2e-3 * (1+z) * np.sqrt(om_m-1) * L
-        x = np.sqrt(om_m-1) * integral(z, om_m)
+
+    def closed_th(z, om_m=3.0, L=20):
+        y = 1.2e-3 * (1 + z) * np.sqrt(om_m - 1) * L
+        x = np.sqrt(om_m - 1) * integral(z, om_m)
         y /= np.sin(x)
         return y
 
--- src/ps5.py
+++ src/ps5.py
@@ -10,32 +10,35 @@ hbar = 1.054571817e-34
 khc = kB / (hbar * c)
 G = 6.67430e-11
 mpc = 3.0856775815e19  # mpc to km
-rho_c = 3750 / (np.pi * G) / mpc**2 * 1e-6 # h^2 kg/cm^3
+rho_c = 3750 / (np.pi * G) / mpc**2 * 1e-6  # h^2 kg/cm^3
 Tcmb = 2.725
-Tnu = Tcmb * (4/11)**(1/3)
+Tnu = Tcmb * (4 / 11) ** (1 / 3)
 m_proton = 1.673e-27
 omega_m = 0.32
 omega_lambda = 0.68
 h = 0.7
 
+
 def n_boson(T, g):
     """
     Number density of relativistic bosons in cm^-3 (hence the 1e6 factor)
     """
-    return g / np.pi**2 * zeta(3) * (khc * T)**3 / 1e6
+    return g / np.pi**2 * zeta(3) * (khc * T) ** 3 / 1e6
+
 
 def u_boson(T, g):
     """
     Energy density of relativistic bosons in J / cm^3
     """
-    return np.pi**2 / 30 * g * (khc * T)**3 * kB * T / 1e6
+    return np.pi**2 / 30 * g * (khc * T) ** 3 * kB * T / 1e6
 
 
 def om_m_z(z, omega_m0):
     """
     Redshift evolution of omega matter
     """
-    return omega_m0 * (z+1)**3
+    return omega_m0 * (z + 1) ** 3
+
 
 def om_l_z(z, omega_l0):
     """
@@ -43,13 +46,15 @@ def om_l_z(z, omega_l0):
     """
     return omega_l0 * np.ones_like(z)
 
+
 def om_r_z(z, omega_r0):
     """
     Redshift evolution of omega radiation
     """
-    return omega_r0 * (z+1)**4
+    return omega_r0 * (z + 1) ** 4
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     # 1a
     n_cmb = n_boson(Tcmb, 2)
     print(f"Number density of CMB photons: {n_cmb:.4g} / cm³")
@@ -67,7 +72,7 @@ if __name__=="__main__":
     # 2d
     m_nu_rel = kB_ev * Tnu
     print(f"Neutrino mass required to be relativistic: {m_nu_rel:.4g} eV.")
-    neutrino_factor = 1 + 7 / (2**(1/3) * 11**(4/3))
+    neutrino_factor = 1 + 7 / (2 ** (1 / 3) * 11 ** (4 / 3))
     print(f"{neutrino_factor=}")
     omega_rad = omega_gamma * neutrino_factor
     print(f"Present radiation density parameter: {omega_rad:.4g}.")
@@ -81,8 +86,8 @@ if __name__=="__main__":
     print(f"{eta_min*beta:.4g} <= omega_b h² <= {eta_max*beta:.4g}")
 
     # 4a
-    zeq = omega_m / (omega_rad/h**2) - 1
-    print(om_r_z(zeq, omega_rad/h**2) - om_m_z(zeq, omega_m))
+    zeq = omega_m / (omega_rad / h**2) - 1
+    print(om_r_z(zeq, omega_rad / h**2) - om_m_z(zeq, omega_m))
     print(f"Matter-radiation equality redshift: {zeq:.4g}.")
 
     # 4b
@@ -90,7 +95,7 @@ if __name__=="__main__":
     redshifts = np.geomspace(1, 1e7, num=100) - 1
     om_ms = om_m_z(redshifts, omega_m)
     om_ls = om_l_z(redshifts, omega_lambda)
-    om_rs = om_r_z(redshifts, omega_rad/h**2)
+    om_rs = om_r_z(redshifts, omega_rad / h**2)
     plt.figure()
     plt.plot(redshifts, om_ms, label="$\Omega_m$")
     plt.plot(redshifts, om_ls, label="$\Omega_\Lambda$")
